825b3b645716facc1fa45effb2c43af27c18985c
kwsantiago <kwsantiago@usf.edu>
2020-10-31 23:51:39 -0400

    Simplify functions

--- frontend/src/assets/utils/smartContracts/genNewContract.js
+++ frontend/src/assets/utils/smartContracts/genNewContract.js
@@ -1,9 +1,8 @@
 const Web3 = require("web3");
 const web3 = new Web3(Web3.givenProvider);
 const { eth } = web3;
-const id = x => x;
-// Set up contract instance
-const abi = [
+
+const factoryABI = [
 	{
 		"inputs": [
 			{
@@ -33,7 +32,7 @@ const abi = [
 ]
 
 const factoryAddress = "0x2378325eB989DAf28D61E0dABd3c52325b20ACe0";
-const factoryContract = new eth.Contract(abi, factoryAddress);
+const factoryContract = new eth.Contract(factoryABI, factoryAddress);
 
 const testAddressList = ["0x6a717a5c747c091AFC5958891c2cd452c7A5beD2","0x6da74A271C51ac4B7B5A81a30059B38D5481FF73","0x033F6B3147eBa5ab0913409D46aC3082FCDb5cF8","0x9C65C5A69e69C67E8e340e893CfCa9A0844d4800","0x0cb510E2F16C36ce039Ee3164330D5F00ECf9eAC","0xe63Cd3474b504435c9F44f8b8135deb6459b32C7"];
 
@@ -52,7 +51,8 @@ async function generateNewContract(){
 
 const newRandomWinnerContract = async () => {
     await generateNewContract();
-    return await factoryContract.methods.randomWinnerAddress().call();
+    const newContract = await factoryContract.methods.randomWinnerAddress().call();
+    return newContract;
 }
 
 export { newRandomWinnerContract };
