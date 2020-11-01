247d88341bdaa918081b753531b87a15c4de2e38
kwsantiago <kwsantiago@usf.edu>
2020-10-31 23:06:50 -0400

    Update frontend function with test

--- contracts/RandomWinner.sol
+++ contracts/RandomWinner.sol
@@ -121,7 +121,7 @@ library SafeMath_Chainlink {
     * - Subtraction cannot overflow.
     */
   function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    require(b <= a, "SafeMath:ubtraction overflow");
+    require(b <= a, "SafeMath:subtraction overflow");
     uint256 c = a - b;
 
     return c;
--- frontend/src/assets/utils/smartContracts/genNewContract.js
+++ frontend/src/assets/utils/smartContracts/genNewContract.js
@@ -5,15 +5,15 @@ const id = x => x;
 // Set up contract instance
 const abi = [
 	{
-		"inputs": [],
-		"name": "createRandomWinner",
-		"outputs": [
+		"inputs": [
 			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
+				"internalType": "address[]",
+				"name": "_addresses",
+				"type": "address[]"
 			}
 		],
+		"name": "createRandomWinner",
+		"outputs": [],
 		"stateMutability": "nonpayable",
 		"type": "function"
 	},
@@ -32,14 +32,16 @@ const abi = [
 	}
 ]
 
-const factoryAddress = "0xb277B2937Ad2c77548DdC3101E6f255DB3cd829a";
+const factoryAddress = "0x2378325eB989DAf28D61E0dABd3c52325b20ACe0";
 const factoryContract = new eth.Contract(abi, factoryAddress);
+
+const testAddressList = ["0x6a717a5c747c091AFC5958891c2cd452c7A5beD2","0x6da74A271C51ac4B7B5A81a30059B38D5481FF73","0x033F6B3147eBa5ab0913409D46aC3082FCDb5cF8","0x9C65C5A69e69C67E8e340e893CfCa9A0844d4800","0x0cb510E2F16C36ce039Ee3164330D5F00ECf9eAC","0xe63Cd3474b504435c9F44f8b8135deb6459b32C7"];
 
 const tx = {
   from: null,
   to: factoryAddress,
   // gas limit for createRandomWinner
-  data: factoryContract.methods.createRandomWinner().encodeABI()
+  data: factoryContract.methods.createRandomWinner(testAddressList).encodeABI()
 };
 
 async function generateNewContract(){
