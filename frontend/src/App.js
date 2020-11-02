606bc40ca61f1d4327be9803eeb4d7e4e214ab78
kwsantiago <kwsantiago@usf.edu>
2020-11-02 12:55:30 -0500

    Update import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import useWeb3 from "./assets/shared/hooks/useWeb3";
-import RandomWinner from "./assets/utils/frontend.js";
+import RandomWinner from "./assets/utils/frontend";
 
 const App = (props) => {
   const { account, web3 } = useWeb3();
